1b9b9fd497a4e9f1752483c9ae47937ffd507734
Yuvraj Soni <demonsroar2172@gmail.com>
2023-06-08 04:43:20 +0530

    mind grenade

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
 console.log(`Hey! This is my first NodeJs program`);
-const amount = 12;
-console.log(amount < 10 ? 'small' : 'large');
+// const amount = 12;
+// console.log(amount < 10 ? 'small' : 'large');
+
+require('./7-mind-grenade');
